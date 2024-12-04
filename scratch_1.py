6bec40de79608a0cf41942db1d728c80392c5d0c
Ilya Yampolskiy <hellboy@niuitmo.ru>
2024-12-04 23:15:29 +0300

    женя хелп

--- scratch_1.py
+++ scratch_1.py
@@ -1,50 +1,54 @@
 class CSR:
     def __init__(self, n: int, m: int, matrix: list[list]):
-        val = []
-        col_ind = []
-        row_ind = [0]
+        self.val = []
+        self.col_ind = []
+        self.row_ind = [0]
         for i in range(n):
             counter = 0
             for j in range(m):
                 if matrix[i][j] != 0:
-                    val.append(matrix[i][j])
-                    col_ind.append(j)
+                    self.val.append(matrix[i][j])
+                    self.col_ind.append(j)
                     counter += 1
-            row_ind.append(row_ind[i] + counter)
-        return [val, col_ind, row_ind]
+            self.row_ind.append(self.row_ind[i] + counter)
 
 
-def csr_to_normal(csr_matrix):
-    col_ind = csr_matrix[1]
-    row_ind = csr_matrix[2]
-    print(max(col_ind))
-    print(len(row_ind))
-    matrix = [[0 for i in range(max(col_ind) + 1)] for j in range(len(row_ind) - 1)]
-    val = csr_matrix[0]
-    for i in range(len(val)):
-        col = col_ind[i]
-        row = 0
-        for j in range(len(row_ind) - 1):
-            if i >= row_ind[j] and i < row_ind[j+1]:
-                row = j
-        matrix[row][col] = val[i]
-    return matrix
+    def csr_to_normal(self):
+        col_ind = self.col_ind
+        row_ind = self.row_ind
+        matrix = [[0 for i in range(max(col_ind) + 1)] for j in range(len(row_ind) - 1)]
+        val = self.val
+        for i in range(len(val)):
+            col = col_ind[i]
+            row = 0
+            for j in range(len(row_ind) - 1):
+                if i >= row_ind[j] and i < row_ind[j+1]:
+                    row = j
+            matrix[row][col] = val[i]
+        return matrix
 
 
 
-def determinant(csr_matrix):
-    normal_matrix = csr_to_normal(csr_matrix)
-    determ = 0
-    if len(normal_matrix) != len(normal_matrix[0]):
-        return "error, not square"
-    if len(normal_matrix) == 2:
-        determ = normal_matrix[0][0] * normal_matrix[1][1] - normal_matrix[1][0] * normal_matrix[0][1]
-    else:
-        for i in range(len(normal_matrix[0])):
-            determ += 0
+    def determinant(self, matrix):
+        normal_matrix = CSR.csr_to_normal(self)
+        determ = 0
+        if len(normal_matrix) != len(normal_matrix[0]):
+            return "error, not square"
+        if len(normal_matrix) == 1:
+            return normal_matrix[0][0]
+        else:
+            col = -1
+            for i in range(len(normal_matrix[0])):
+                col += 1
+                for j in range(len(normal_matrix)):
+                    new_matrix = normal_matrix[:j] + normal_matrix[j + 1:]
+                    determ += normal_matrix[i] * CSR.determinant(self, new_matrix)
 
 
-n, m = map(int, input().split())
-matrix = [list(map(float, input().split())) for i in range(n)]
-csr = CSR(n, m, matrix)
-print(csr_to_normal(csr.create_matrix()))
+
+
+my_n, my_m = map(int, input().split())
+my_matrix = [list(map(float, input().split())) for i in range(my_n)]
+csr = CSR(my_n, my_m, my_matrix)
+csr.determinant()
+print(my_matrix.__init__())
