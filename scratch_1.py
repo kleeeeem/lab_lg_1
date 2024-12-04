bf4c8ba31d84125a43dc2ec8269e7a75246b98b9
Ilya Yampolskiy <hellboy@niuitmo.ru>
2024-12-04 20:23:06 +0300

    meow

--- scratch_1.py
+++ scratch_1.py
@@ -1,9 +1,8 @@
 class csr:
     def create_matrix():
-        m = int(input())
         n = int(input())
+        m = int(input())
         val = []
-        trace = 0
         col_ind = []
         row_ind = [0]
         matrix = list()
@@ -20,5 +19,33 @@ class csr:
         return [val, col_ind, row_ind]
 
 
-print(csr.create_matrix())
-#meow
+
+
+
+def csr_to_normal(csr_matrix):
+    col_ind = csr_matrix[1]
+    row_ind = csr_matrix[2]
+    print(max(col_ind))
+    print(len(row_ind))
+    matrix = [[0 for i in range(max(col_ind) + 1)] for j in range(len(row_ind) - 1)]
+    val = csr_matrix[0]
+    for i in range(len(val)):
+        col = col_ind[i]
+        row = 0
+        for j in range(len(row_ind) - 1):
+            if i >= row_ind[j] and i < row_ind[j+1]:
+                row = j
+        matrix[row][col] = val[i]
+    return matrix
+
+
+
+def determinant(csr_matrix):
+    normal_matrix = csr_to_normal(csr_matrix)
+    if len() != len(csr_matrix[2] - 1):
+        return "error, not square"
+
+
+
+
+print(csr_to_normal(csr.create_matrix()))
