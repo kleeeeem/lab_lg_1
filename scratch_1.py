bb4756dbf3feaccc44021596309fe5983a4bb53e
Ilya Yampolskiy <hellboy@niuitmo.ru>
2024-12-04 20:58:42 +0300

    meow

--- scratch_1.py
+++ scratch_1.py
@@ -1,13 +1,8 @@
-class csr:
-    def create_matrix():
-        n = int(input())
-        m = int(input())
+class CSR:
+    def __init__(self, n: int, m: int, matrix: list[list]):
         val = []
         col_ind = []
         row_ind = [0]
-        matrix = list()
-        for iterations in range(n):
-            matrix += [list(map(int, input().split()))]
         for i in range(n):
             counter = 0
             for j in range(m):
@@ -17,9 +12,6 @@ class csr:
                     counter += 1
             row_ind.append(row_ind[i] + counter)
         return [val, col_ind, row_ind]
-
-
-
 
 
 def csr_to_normal(csr_matrix):
@@ -42,10 +34,17 @@ def csr_to_normal(csr_matrix):
 
 def determinant(csr_matrix):
     normal_matrix = csr_to_normal(csr_matrix)
-    if len() != len(csr_matrix[2] - 1):
+    determ = 0
+    if len(normal_matrix) != len(normal_matrix[0]):
         return "error, not square"
+    if len(normal_matrix) == 2:
+        determ = normal_matrix[0][0] * normal_matrix[1][1] - normal_matrix[1][0] * normal_matrix[0][1]
+    else:
+        for i in range(len(normal_matrix[0])):
+            determ += 0
 
 
-
-
+n, m = map(int, input().split())
+matrix = [list(map(float, input().split())) for i in range(n)]
+csr = CSR(n, m, matrix)
 print(csr_to_normal(csr.create_matrix()))
